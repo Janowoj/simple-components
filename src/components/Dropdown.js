2d83a30002148264329b8dba006f979b71f10615
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-10-16 22:41:08 +0200

    controlled component implementation

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, onSelect }) {
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -9,19 +9,29 @@ function Dropdown({ options }) {
         setIsOpen(!isOpen);
     }
 
+
     const handleOptionClick = (option) => {
         // CLOSE DROPDOWN
         setIsOpen(false);
         // DISPLAY WHAT DID THE USER CLICK ON? instead of event object we can pass the option object
-        console.log(option)
+        onSelect(option);
     }
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+        return (
+            <div onClick={() => handleOptionClick(option)} key={option.value}>
+                {option.label}
+            </div>
+        )
     });
 
+    let content = 'Select...';
+    if (selection) {
+        content = selection.label;
+    }
+
     return <div>
-        <div onClick={handleClick}>Select...</div>
+        <div onClick={handleClick}>{content}</div>
         {isOpen && <div>{renderedOptions}</div>}
     </div>
 }

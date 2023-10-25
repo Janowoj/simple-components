3ff9f49e247455ad2a4a8f5a1bb1fbd80a27cdc4
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-10-25 22:52:57 +0200

    added mapping function on data to receive table body on Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,15 @@
 function Table({ data }) {
+
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        )
+    })
+
     return (
         <table>
             <thead>
@@ -9,26 +20,7 @@ function Table({ data }) {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>apple</td>
-                    <td>red</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>banana</td>
-                    <td>yellow</td>
-                    <td>2</td>
-                </tr>
-                <tr>
-                    <td>grape</td>
-                    <td>purple</td>
-                    <td>4</td>
-                </tr>
-                <tr>
-                    <td>orange</td>
-                    <td>orange</td>
-                    <td>3</td>
-                </tr>
+                {renderedRows}
             </tbody>
         </table>
     )

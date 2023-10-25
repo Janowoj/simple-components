cdf1108744dbc710bd9e72eb8173da7bec2137fc
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-10-25 22:35:25 +0200

    added a table jsx element with some data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,37 @@
 function Table({ data }) {
-    return <div>
-        {data.length}
-    </div>
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>fruit</th>
+                    <th>color</th>
+                    <th>score</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>apple</td>
+                    <td>red</td>
+                    <td>5</td>
+                </tr>
+                <tr>
+                    <td>banana</td>
+                    <td>yellow</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>grape</td>
+                    <td>purple</td>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <td>orange</td>
+                    <td>orange</td>
+                    <td>3</td>
+                </tr>
+            </tbody>
+        </table>
+    )
 }
 
 export default Table;

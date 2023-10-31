daa5c77abeef78ac9a266fcd90082ad8c294a8d4
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-10-31 23:24:33 +0100

    added sordedData variable and sort() method on it and passef to the Table component without arrows

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -7,7 +7,7 @@ function SortableTable(props) {
 
     const [sortBy, setSortBy] = useState(null);
 
-    const { config } = props;
+    const { config, data } = props;
 
     const handleClick = (label) => {
         if (sortOrder === null) {
@@ -33,9 +33,34 @@ function SortableTable(props) {
         }
     })
 
+    // Only sort data if sortOrder && sortBy are not null
+
+    // Make a copy of the 'data' prop (never modify an array if an array is a part of the prop system or the state system)
+
+    // Find the correct sortValue function and use it for sorting
+
+    let sortedData = data;
+
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find(column => column.label === sortBy);
+
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        })
+    }
+
     return <div>
         {sortOrder} - {sortBy}
-        <Table {...props} config={updatedConfig} />
+        <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
 
 }
